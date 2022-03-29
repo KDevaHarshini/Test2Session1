0b26972611193800193ad0fcbf179d061d6799d4
kancharla devaharshini <94678326+KDevaHarshini@users.noreply.github.com>
2022-03-29 11:08:34 +0000

    p3 committed

--- p3original.c
+++ p3original.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<math.h>
+#include <math.h>
 int input_number()
 {
   int n;
@@ -7,24 +7,23 @@ int input_number()
   scanf("%d",&n);
   return n;
 }
-
 int is_prime(int n)
 {
   if (n==0 || n==1)
   {
     return 0;
-  }
+  }  
   if (n==2)
   {
     return 1;
-  }
+  }  
   for(int i = 2; i<=sqrt(n);i++)
     {
       if(n%i==0)
         return 0;
     }
   return 1;
-  }
+}
 void output(int n, int is_prime)
 {
   if(is_prime == 0)
